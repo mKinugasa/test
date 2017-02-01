e1cb9c4042d3cdc8bcbf3b6da00de7e29d7f2d2d
Masaya kinugasa <masaya.kinugasa@gmail.com>
2017-02-01 09:24:57 +0900

    Add password reset

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,36 @@
 Rails.application.routes.draw do
-  get 'users/new'
 
-  root 'static_pages#home'
-  get  '/help',    to: 'static_pages#help'
-  get  '/about',   to: 'static_pages#about'
-  get  '/contact', to: 'static_pages#contact'
-  get  '/signup',  to: 'users#new'
-  post '/signup',  to: 'users#create'
+  root   'static_pages#home'
+  get    '/help',    to: 'static_pages#help'
+  get    '/about',   to: 'static_pages#about'
+  get    '/contact', to: 'static_pages#contact'
+  get    '/signup',  to: 'users#new'
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
   resources :users
   resources :account_activations, only: [:edit]
-  get  'static_pages/home'
-  get  'static_pages/help'
-  get  'static_pages/about'
-  get  'static_pages/contact'
+  resources :password_resets,     only: [:new, :create, :edit, :update]
+
+  # get 'password_resets/new'
+  #
+  # get 'password_resets/edit'
+  #
+  # get 'users/new'
+  #
+  # root 'static_pages#home'
+  # get  '/help',    to: 'static_pages#help'
+  # get  '/about',   to: 'static_pages#about'
+  # get  '/contact', to: 'static_pages#contact'
+  # get  '/signup',  to: 'users#new'
+  # post '/signup',  to: 'users#create'
+  # get    '/login',   to: 'sessions#new'
+  # post   '/login',   to: 'sessions#create'
+  # delete '/logout',  to: 'sessions#destroy'
+  # resources :users
+  # resources :account_activations, only: [:edit]
+  # get  'static_pages/home'
+  # get  'static_pages/help'
+  # get  'static_pages/about'
+  # get  'static_pages/contact'
 end
